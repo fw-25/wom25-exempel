bc3cb5a94d3114f2211faea3553a2aaec93cfa47
Fredrik Welander <welandfr@arcada.fi>
2025-09-10 15:31:20 +0300

    mellancommit 2

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,47 @@
 const express = require('express')
 const router = express.Router()
 
+// ersätts senare med riktig data från DB
+const tempData = [
+    { text: "hej" },
+    { text: "morjens"}
+]
+
 router.get('/', (req, res) => {
-    res.send("Notes!")
+    res.send(tempData)
+})
+
+router.post('/', (req, res) => {
+    console.log("POST")
+
+    // simulering av SQL INSERT INTO....
+    tempData.push(req.body)
+
+    res.send({ 
+        method: req.method, 
+        body: req.body
+    })
+})
+
+router.put('/:id', (req, res) => {
+    // SQL: UPDATE ... WHERE id = :id
+    tempData[req.params.id] = req.body
+        
+    res.send({ 
+        method: req.method, 
+        body: req.body
+    })
+
+})
+
+router.delete('/:id', (req, res) => {
+    // SQL: DELETE FROM notes WHERE id = :id
+    tempData.splice(req.params.id)
+    res.send({ 
+        method: req.method, 
+        msg: `Deleted ${req.params.id}`
+    })
+
 })
 
 module.exports = router
